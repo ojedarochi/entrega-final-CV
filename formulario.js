8f53212f8ead08d9144cbe8a9f3e98937aa67023
ojedarochi <109388161+ojedarochi@users.noreply.github.com>
2023-02-28 23:04:30 -0300

    Add files via upload

---
+++ formulario.js
@@ -0,0 +1,21 @@
+document.getElementById('boton-enviar').onclick = function() {
+    document.getElementById('boton-enviar').innerHTML = "¡Enviado!";
+}
+
+document.getElementById('card1').onmouseover = function() {
+    document.getElementById('card1').style.backgroundColor = '#84B13F';
+}
+
+document.getElementById('card2').onmouseover = function() {
+    document.getElementById('card2').style.backgroundColor = '#D0437C';
+}
+
+document.getElementById('card3').onmouseover = function() {
+    document.getElementById('card3').style.backgroundColor = '#3B59B4';
+}
+
+document.getElementById('card4').onmouseover = function() {
+    document.getElementById('card4').style.backgroundColor = '#C3BA58';
+}
+
+
